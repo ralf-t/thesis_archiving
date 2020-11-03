2f997185cd68fba4720cf9927d1fa8ef7f247cf0
ralf-t <tabo.ralfmartin@ue.edu.ph>
2020-11-03 21:15:14 +0800

    mod

--- thesisarchiving/static/admin/custom js/register_general.js
+++ thesisarchiving/static/admin/custom js/register_general.js
@@ -1,13 +1,13 @@
 $( document ).ready(function() {
 
 	$("#select_data").change(function(){
-		if($("#select_data").val() == "Subject") {
-				$("#name").show();
+		if($("#select_data").val() == "Subject"){
+			$("#name").show();
 		}
-		else if($("#select_data").val() == "Section") {
-				$("#name").val("");
-				$("#name").hide();
-			}
+		else{
+			$("#name").val("");
+			$("#name").hide();
+		}
 	});
 });	
 
